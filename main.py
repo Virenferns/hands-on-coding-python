93b8d9a450f8c7329875c924c9ec72e814bc1a29
Viren Fernandes <fernandesviren10@gmail.com>
2023-03-13 16:12:30 +0000

    code added to main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def main(list, k):
+    output = []
+    for i in range(0, (len(list) - k)+1):
+        total_value = sum(list[i:k])
+        k += 1
+        output.append(total_value)
+    return max(output)
+
+print(main([-3,4,3,-2,2,5], 4))
